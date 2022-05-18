d077b7f130db5e09f09c2492fb21455c73d19dd1
Samir Cabrera Moreno <sammy_cabrera@hotmail.com>
2022-05-17 23:32:55 -0500

    Jasypt

--- src/main/java/co/com/gpc/mail/receiver/MailReceiverApplication.java
+++ src/main/java/co/com/gpc/mail/receiver/MailReceiverApplication.java
@@ -1,5 +1,6 @@
 package co.com.gpc.mail.receiver;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Sammy
  */
 @SpringBootApplication
+@EnableEncryptableProperties
 public class MailReceiverApplication {
 
     
